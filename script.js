5a6c9e1c8352f524abc808528e5de94b8423231e
aleksandrsg <aleksandrs.galiullins@gmail.com>
2021-08-17 22:52:43 +0100

    Anagram

--- script.js
+++ script.js
@@ -5,26 +5,69 @@
 
 //FizzBuzz 
 
-let out = document.getElementById('out');
+// let out = document.getElementById('out');
 
-let tmp ='';
+// let tmp ='';
 
-fizzBuzz(30);
+// fizzBuzz(30);
 
-function fizzBuzz(num){
-    for (let i=1; i<=num; i++){
-        if (i % 3 === 0 && i % 5 ===0){
-            tmp = tmp + 'FizzBuzz' +  '</br>';
-        }
-        else if (i % 3 === 0){
-            tmp = tmp + 'Fizz' + '</br>';
-        }
-        else if (i % 5 === 0) {
-            tmp = tmp + 'Buzz' + '</br>';
-        }
-        else {
-            tmp = tmp + i + '</br>';
+// function fizzBuzz(num){
+//     for (let i=1; i<=num; i++){
+//         if (i % 3 === 0 && i % 5 ===0){
+//             tmp = tmp + 'FizzBuzz' +  '</br>';
+//         }
+//         else if (i % 3 === 0){
+//             tmp = tmp + 'Fizz' + '</br>';
+//         }
+//         else if (i % 5 === 0) {
+//             tmp = tmp + 'Buzz' + '</br>';
+//         }
+//         else {
+//             tmp = tmp + i + '</br>';
+//         }
+//     }
+// }
+// out.innerHTML= tmp;
+
+
+//Anagram
+
+// anagram('friend', 'Finder'); // will be true
+// anagram('hi', 'bye'); // will be false
+
+//OneLiner Code
+
+// const anagram = (a,b) => [...a.toLowerCase()].sort().toString() === [...b.toLowerCase()].sort().toString();
+// let out = document.getElementById('out');
+// out.innerHTML=anagram('friend', 'Finder');
+
+//Anagram2
+
+function strbuildCharObject(str){
+    const charObj = {};
+    str = str.toLowerCase();
+    for (let char of str ){
+        charObj[char]=charObj[char]+1 || 1;
+    }
+
+    return charObj;
+}
+
+
+function anagram (str1, str2){
+    const aCharObj = strbuildCharObject(str1);
+    const bCharObj = strbuildCharObject(str2);
+
+    if (Object.keys(aCharObj).length !== Object.keys(bCharObj).length){
+        return false;
+    }
+
+    for (let char in aCharObj){
+        if(aCharObj[char]!== bCharObj[char]){
+            return false;
         }
     }
+    return true;
 }
-out.innerHTML= tmp;
+
+console.log(anagram('yes','no'));
